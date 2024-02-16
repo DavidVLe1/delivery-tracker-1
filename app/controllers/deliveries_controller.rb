73df9c0a4007ae2fc0a4c5aa26ad825ff6587956
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-16 06:48:14 +0000

    added modify delivery functionality. removed attribute accessor model method.

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -42,17 +42,17 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.arrived = params.fetch("query_arrived", false)
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    the_delivery.user_id = current_user.id
+    the_delivery.arrived = params.fetch("arrived")
+    the_delivery.description = params.fetch("description")
+    the_delivery.supposed_to_arrive_on = Date.parse(params.fetch("query_supposed_to_arrive_on"))
     the_delivery.details = params.fetch("query_details")
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
